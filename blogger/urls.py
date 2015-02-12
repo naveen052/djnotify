c6bdc103e62358834c5b0c2258fe962d33349660
Naveen Singh <naveensingh.org@gmail.com>
2015-02-12 11:44:21 +0530

    able to edit posts selected with primary key

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from content.views import PostList
 
 urlpatterns = patterns('',
                        # Examples:
@@ -7,8 +8,11 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', 'content.views.post_list', name='index'),
+                       url(r'^$', PostList.as_view(), name='index'),
                        url(r'^addpost/$',
                            'content.views.add_content',
                            name='add_content'),
+                       url(r'^(?P<pk>[0-9]+)/edit/$',
+                           'content.views.edit_post',
+                           name='edit'),
 )
--- content/views.py
+++ content/views.py
@@ -1,19 +1,33 @@
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import permission_required, login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render_to_response, render
+from django.db.models.signals import post_syncdb
+from django.shortcuts import render_to_response, render, redirect, get_object_or_404
 from django.template import RequestContext
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+
 from content.forms import BlogForm
 from content.models import BlogContent
 
 
-@permission_required('content.post_list', login_url='/admin/login/')
-def post_list(request):
-    context = {'posts': BlogContent.objects.all().order_by('-created'),
-               'all_users': User.objects.all()}
-    return render(request, 'index.html', context)
+class PostList(View):
+    template_name = 'index.html'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        context = {
+            "posts": BlogContent.objects.all().order_by('-created'),
+            "all_users": User.objects.all()
+        }
+        return render(request, self.template_name, context)
+
+    post_syncdb.connect(get)
 
 '''
-
+def post_list(self):
+        context = {'posts': BlogContent.objects.all().order_by('-created'),
+                   'all_users': User.objects.all()}
+        return render(request, 'index.html', context)
 def post_list(request, year):
     posts = BlogContent.objects.values(created__year=year)
 
@@ -32,11 +46,27 @@ def index(request):
 @permission_required('content.post_list', login_url='/admin/login/')
 def add_content(request):
 
-    form = BlogForm(request.POST or None)
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.PostList', pk=post.pk)
+    else:
+        form = BlogForm()
+    return render(request, 'form.html', {'form': form})
 
-    if form.is_valid():
-        save = form.save(commit=False)
-        save.save()
 
-    return render_to_response("form.html", locals(),
-                              context_instance=RequestContext(request))
+def edit_post(request, pk):
+    post = get_object_or_404(BlogContent, pk=pk)
+    if request.method == "POST":
+        form = BlogForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('content.views.PostList', pk=post.pk)
+    else:
+        form = BlogForm(instance=post)
+    return render(request, 'edit.html', {'form': form})
